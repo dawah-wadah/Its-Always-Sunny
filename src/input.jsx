d7fa559bb7c0c74d4655aa36cad45ab8cf85130a
Wadah Adlan <wadahadlan@gmail.com>
2017-09-12 14:17:58 -0400

    Creating Input Field

---
+++ src/input.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class Input extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      video: '',
+      start: 0,
+      end: 1,
+      title: ''
+    };
+  }
+
+  _onChange(e){
+    e.currentTarget();
+  }
+
+  render(){
+
+  }
+}
