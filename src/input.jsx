fc236809d040a3d57fd37a0ff38ac61de49a09d6
Wadah Adlan <wadahadlan@gmail.com>
2017-09-13 12:29:46 -0400

    Working on functionality

--- src/input.jsx
+++ src/input.jsx
@@ -9,10 +9,22 @@ class Input extends Component {
 			end: null,
 			title: ""
 		};
+
+		this._onChange = this._onChange.bind(this);
 	}
 
-	_onChange(e) {
-		e.currentTarget();
+	_onChange(e, field) {
+		this.setState({[field] : e.currentTarget.value});
+	}
+
+	_handleSubmit(e){
+
+		debugger
+		console.log('clicked');
+	}
+
+	_getUrl(url){
+		return url.split('v=').pop();
 	}
 
 	render() {
@@ -20,26 +32,29 @@ class Input extends Component {
 			<div className="input-field">
 					<div className="row">
 						<div className="input-text">
-							<input id="vidURL" type="text" className="input" />
+							<input id="vidURL" type="text" className="input" onChange={(e) => this._onChange(e, 'video')}/>
 							<label htmlFor="vidURL"> URL of a Youtube Video</label>
 						</div>
 					</div>
 					<div className="row">
 						<div className="input-text">
-							<input id="start" type="text" className="input" />
+							<input id="start" type="text" className="input" onChange={(e) => this._onChange(e, 'start')}/>
 							<label htmlFor="start"> Start Time</label>
 						</div>
 						<div className='spacer'></div>
 						<div className="input-text">
-							<input id="end" type="text" className="input" />
+							<input id="end" type="text" className="input" onChange={(e) => this._onChange(e, 'end')} />
 							<label htmlFor="end"> End Time</label>
 						</div>
 					</div>
 					<div className="row">
 						<div className="input-text">
-							<input id="Title" type="text" className="input" />
+							<input id="Title" type="text" className="input" onChange={(e) => this._onChange(e, 'title')} />
 							<label htmlFor="Title"> Title</label>
 						</div>
+					</div>
+					<div className='row'>
+						<div className='button' onClick={this._handleSubmit.bind(this)}>Generate</div>
 					</div>
 			</div>
 		);
