15d7529f78b5e242dc3ea32f5f40d7764f01e89c
Wadah Adlan <wadahadlan@gmail.com>
2017-09-12 21:32:27 -0400

    format

--- src/input.jsx
+++ src/input.jsx
@@ -18,27 +18,30 @@ class Input extends Component {
 	render() {
 		return (
 			<div className="input-field">
-				<div className="form test">
+				<form className="column">
 					<div className="row">
-						<p>test</p>
-					</div>
-					<div className="row">
-						<div className="column">
-							<p>test 2</p>
-						</div>
-						<div className="column">
-							<p>test 3</p>
+						<div className="input-text">
+							<input id="vidURL" type="text" className="input" />
+							<label htmlFor="vidURL"> URL of a Youtube Video</label>
 						</div>
 					</div>
 					<div className="row">
-						<div className="column">
-							<p>test 4</p>
+						<div className="input-text">
+							<input id="start" type="text" className="input" />
+							<label htmlFor="start"> Start Time</label>
+						</div>
+						<div className="input-text">
+							<input id="end" type="text" className="input" />
+							<label htmlFor="end"> End Time</label>
 						</div>
 					</div>
 					<div className="row">
-						<p>test 5</p>
+						<div className="input-text">
+							<input id="Title" type="text" className="input" />
+							<label htmlFor="Title"> Titleo</label>
+						</div>
 					</div>
-				</div>
+				</form>
 			</div>
 		);
 	}
